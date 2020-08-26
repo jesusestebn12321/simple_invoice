38d22d3e1ab172f02b45a304a843f4d68ca25ac7
Jayden Pollard <5141032-JaydenPollard98789@users.noreply.gitlab.com>
2020-08-26 02:11:28 -0400

    validaciones para los stock y agg toarts para las notificaciones

--- js/nueva_factura.js
+++ js/nueva_factura.js
@@ -5,9 +5,10 @@
 
 		function load(page){
 			var q= $("#q").val();
+			var id_categoria=$('#id_categoria').val();
 			$("#loader").fadeIn('slow');
 			$.ajax({
-				url:'./ajax/productos_factura.php?action=ajax&page='+page+'&q='+q,
+				url:'./ajax/productos_factura.php?action=ajax&page='+page+'&q='+q+'&id_categoria='+id_categoria,
 				 beforeSend: function(objeto){
 				 $('#loader').html('<img src="./img/ajax-loader.gif"> Cargando...');
 			  },
@@ -19,67 +20,73 @@
 			})
 		}
 
-	function agregar (id)
-		{
-			var precio_venta=document.getElementById('precio_venta_'+id).value;
-			var cantidad=document.getElementById('cantidad_'+id).value;
-			//Inicia validacion
-			if (isNaN(cantidad))
-			{
-			alert('Esto no es un numero');
+	function agregar (id,stock){
+		var precio_venta=document.getElementById('precio_venta_'+id).value;
+		var cantidad=$('#cantidad_'+id).val();
+		//Inicia validacion
+		if(stock==0){
+			console.log('el stock es menor a la cantidad');
+			toastr['error']('El producto no tiene stock.','Error');
 			document.getElementById('cantidad_'+id).focus();
 			return false;
-			}
-			if (isNaN(precio_venta))
-			{
-			alert('Esto no es un numero');
+		}
+		if(cantidad==0){
+			document.getElementById('cantidad_'+id).focus();
+			toastr['error']('No tiene una cantidad asignada.','Error');
+			return false;
+		}
+		if(isNaN(cantidad)){
+			toastr['error']('Esto no es un numero','Error');
+			document.getElementById('cantidad_'+id).focus();
+			return false;
+		}
+		if(isNaN(precio_venta)){
+			toastr['error']('Esto no es un numero','Error');
 			document.getElementById('precio_venta_'+id).focus();
 			return false;
+		//Fin validacion
+		}		
+		$.ajax({
+	        type: "POST",
+	        url: "./ajax/agregar_facturacion.php",
+	        data: "id="+id+"&precio_venta="+precio_venta+"&cantidad="+cantidad,
+			beforeSend: function(objeto){
+				$("#resultados").html("Mensaje: Cargando...");
+			},
+	        success: function(datos){
+				$("#resultados").html(datos);
+				toastr['success']('Se agrego un producto con exito.','Exito');
 			}
-			//Fin validacion
+		});
 			
-			$.ajax({
-        type: "POST",
-        url: "./ajax/agregar_facturacion.php",
-        data: "id="+id+"&precio_venta="+precio_venta+"&cantidad="+cantidad,
-		 beforeSend: function(objeto){
-			$("#resultados").html("Mensaje: Cargando...");
-		  },
-        success: function(datos){
-		$("#resultados").html(datos);
-		}
-			});
-		}
+	}
 		
-			function eliminar (id)
-		{
-			
-			$.ajax({
-        type: "GET",
-        url: "./ajax/agregar_facturacion.php",
-        data: "id="+id,
-		 beforeSend: function(objeto){
-			$("#resultados").html("Mensaje: Cargando...");
-		  },
-        success: function(datos){
-		$("#resultados").html(datos);
-		}
-			});
-
-		}
-		
-		$("#datos_factura").submit(function(){
-		  var id_cliente = $("#id_cliente").val();
-		  var id_vendedor = $("#id_vendedor").val();
-		  var condiciones = $("#condiciones").val();
+	function eliminar (id){
+		$.ajax({
+	        type: "GET",
+	        url: "./ajax/agregar_facturacion.php",
+	        data: "id="+id,
+			beforeSend: function(objeto){
+				$("#resultados").html("Mensaje: Cargando...");
+		  	},
+        	success: function(datos){
+				$("#resultados").html(datos);
+			}
+		});
+	}	
+	
+	$("#datos_factura").submit(function(){
+		var id_cliente = $("#id_cliente").val();
+		var id_vendedor = $("#id_vendedor").val();
+		var condiciones = $("#condiciones").val();
 		  
-		  if (id_cliente==""){
-			  alert("Debes seleccionar un cliente");
-			  $("#nombre_cliente").focus();
-			  return false;
-		  }
-		 VentanaCentrada('./pdf/documentos/factura_pdf.php?id_cliente='+id_cliente+'&id_vendedor='+id_vendedor+'&condiciones='+condiciones,'Factura','','1024','768','true');
-	 	});
+		if (id_cliente==""){
+			alert("Debes seleccionar un cliente");
+			$("#nombre_cliente").focus();
+			return false;
+		}
+		VentanaCentrada('./pdf/documentos/factura_pdf.php?id_cliente='+id_cliente+'&id_vendedor='+id_vendedor+'&condiciones='+condiciones,'Factura','','1024','768','true');
+	});
 		
 		$( "#guardar_cliente" ).submit(function( event ) {
 		  $('#guardar_datos').attr("disabled", true);
