cab275d32c6e5ccef5d635048c7fa2142d9fc893
Jayden Pollard <5141032-JaydenPollard98789@users.noreply.gitlab.com>
2020-08-26 02:13:32 -0400

    buscar productos por categorias

--- ajax/productos_factura.php
+++ ajax/productos_factura.php
@@ -9,12 +9,13 @@
 	$action = (isset($_REQUEST['action'])&& $_REQUEST['action'] !=NULL)?$_REQUEST['action']:'';
 	if($action == 'ajax'){
 		// escaping, additionally removing everything that could be (html/javascript-) code
-         $q = mysqli_real_escape_string($con,(strip_tags($_REQUEST['q'], ENT_QUOTES)));
-		 $aColumns = array('codigo_producto', 'nombre_producto', 'proveedor_producto', 'descripcion_producto');//Columnas de busqueda
-		 $sTable = "products";
-		 $sWhere = "";
-		if ( $_GET['q'] != "" )
-		{
+        $q = mysqli_real_escape_string($con,(strip_tags($_REQUEST['q'], ENT_QUOTES)));
+        $id_categoria =intval($_REQUEST['id_categoria']);
+		$aColumns = array('codigo_producto', 'nombre_producto', 'proveedor_producto', 'descripcion_producto');//Columnas de busqueda
+		 
+		$sTable = "products";
+		$sWhere = "";
+		if ( $_GET['q'] != "" ){
 			$sWhere = "WHERE (";
 			for ( $i=0 ; $i<count($aColumns) ; $i++ )
 			{
@@ -22,7 +23,11 @@
 			}
 			$sWhere = substr_replace( $sWhere, "", -3 );
 			$sWhere .= ')';
+			if ($id_categoria>0){
+				$sWhere .=" and id_categoria='$id_categoria'";
+			}
 		}
+
 		include 'pagination.php'; //include pagination file
 		//pagination variables
 		$page = (isset($_REQUEST['page']) && !empty($_REQUEST['page']))?$_REQUEST['page']:1;
@@ -80,7 +85,7 @@
 						<td class='col-xs-2'><div class="pull-right">
 						<input type="text" class="form-control" style="text-align:right" id="precio_venta_<?php echo $id_producto; ?>"  value="<?php echo $precio_venta;?>" >
 						</div></td>
-						<td class='text-center'><a class='btn btn-info'href="#" onclick="agregar('<?php echo $id_producto ?>')"><i class="glyphicon glyphicon-plus"></i></a></td> 
+						<td class='text-center'><a class='btn btn-info'href="#" onclick="agregar('<?php echo $id_producto ?>','<?php echo $cantidad; ?>')"><i class="glyphicon glyphicon-plus"></i></a></td> 
 						
 
 					</tr>
