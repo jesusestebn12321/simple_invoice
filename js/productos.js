7774ee135a286a3c2a5a1ba8db448594338b1190
Jayden Pollard <5141032-JaydenPollard98789@users.noreply.gitlab.com>
2020-08-26 03:17:32 -0400

    funcion pop_up para los pdf productos

--- js/productos.js
+++ js/productos.js
@@ -41,6 +41,18 @@
 			});
 		}
 		}
+	function VentanaCentrada(theURL,winName,features, myWidth, myHeight, isCenter) { //v3.0
+		if(window.screen)if(isCenter)if(isCenter=="true"){
+			var myLeft = (screen.width-myWidth)/2;
+			var myTop = (screen.height-myHeight)/2;
+			features+=(features!='')?',':'';
+			features+=',left='+myLeft+',top='+myTop;
+		}
+		window.open(theURL,winName,features+((features!='')?',':'')+'width='+myWidth+',height='+myHeight);
+	}
+	$('#imprimir').click(function(){
+		VentanaCentrada('./pdf/documentos/all_products.php?Productos','','1024','768','true');
+	});
 		
 		
 		
