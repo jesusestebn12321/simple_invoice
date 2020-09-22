57239a12b127b2d796221d49227af091b179ca65
Jayden Pollard <jesusesteban12321@gmail.com>
2020-09-22 01:28:28 -0400

    (feature) crear proveedor terminado con exito

--- ajax/crear_proveedor.php
+++ ajax/crear_proveedor.php
@@ -14,16 +14,23 @@
 		require_once ("../config/db.php");//Contiene las variables de configuracion para conectar a la base de datos
 		require_once ("../config/conexion.php");//Contiene funcion que conecta a la base de datos
 		// escaping, additionally removing everything that could be (html/javascript-) code
-		$nombre_empresa=mysqli_real_escape_string($con,(strip_tags($_POST["nombre_empresa"],ENT_QUOTES)));
+
 		$numero_impusto=mysqli_real_escape_string($con,(strip_tags($_POST["numero_impusto"],ENT_QUOTES)));
+		
 		$sitio_web=mysqli_real_escape_string($con,(strip_tags($_POST["sitio_web"],ENT_QUOTES)));
+		
 		$telefono_empresa=mysqli_real_escape_string($con,(strip_tags($_POST["telefono_empresa"],ENT_QUOTES)));
+
 		$nombre_contacto=mysqli_real_escape_string($con,(strip_tags($_POST["nombre_contacto"],ENT_QUOTES)));
 		$apellido_contacto=mysqli_real_escape_string($con,(strip_tags($_POST["apellido_contacto"],ENT_QUOTES)));
+		
 		$email=mysqli_real_escape_string($con,(strip_tags($_POST["email"],ENT_QUOTES)));
+		
 		$telefono=mysqli_real_escape_string($con,(strip_tags($_POST["telefono"],ENT_QUOTES)));
+
 		$direccion=mysqli_real_escape_string($con,(strip_tags($_POST["direccion"],ENT_QUOTES)));
 		
+		$nombre_empresa=mysqli_real_escape_string($con,(strip_tags($_POST["nombre_empresa"],ENT_QUOTES)));
 		
 
 
@@ -40,15 +47,14 @@
 		}
 		
 		if (isset($errors)){
-			
-			?>
-			
-			<?php
+		foreach ($errors as $value) {
+			    echo $value;
+			}	
+		}
+		if (isset($messages)){
+			foreach ($messages as $value) {
+			    echo $value;
 			}
-			if (isset($messages)){
-				
-			
-				
-			}
+		}
 
 ?>
