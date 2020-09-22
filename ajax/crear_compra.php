7895ecf1ff07b9c2d5deda0056b5b958ad99f1d7
Jayden Pollard <jesusesteban12321@gmail.com>
2020-09-22 01:28:05 -0400

    (feature) crear compra terminado con exito

--- ajax/crear_compra.php
+++ ajax/crear_compra.php
@@ -10,10 +10,7 @@
 		// escaping, additionally removing everything that could be (html/javascript-) code
 			
 		/*mysqli_query($con,$query_producto);*/
-
-
 		try {
-			
 			$id_proveedor=intval($_POST['id_proveedor']);
 			$query_proveedor="SELECT * FROM proveedores WHERE id=$id_proveedor";
 			$show_proveedor=mysqli_query($con,$query_proveedor);
@@ -26,8 +23,13 @@
 			$iva=mysqli_real_escape_string($con,(strip_tags($_POST["iva"],ENT_QUOTES)));
 			$id_user=$_SESSION['user_id'];
 
-			$query_historial="INSERT INTO historial_compras (fecha, neto, iva, id_user, proveedor) VALUES ('$fecha', '$neto', '$iva','$id_user','$proveedor')";
-			$query_new_insert = mysqli_query($con,$query_historial);
+			$sql_new_historial="INSERT INTO historial_compras (fecha, neto, iva, id_user, proveedor) VALUES ('$fecha', '$neto', '$iva','$id_user','$proveedor')";
+			$query_new_historial = mysqli_query($con,$sql_new_historial);
+
+			$sql_show_historial="SELECT MAX(ID) AS ultimo_registro FROM historial_compras";
+			$query_show_historial= mysqli_query($con,$sql_show_historial);
+			$id_historial_compra=mysqli_fetch_array($query_show_historial)['ultimo_registro'];
+
 			
 			$contador=$_POST['cont'];
 			for ($i=0; $i < $contador; $i++) { 
@@ -37,7 +39,7 @@
 				$query_productos=mysqli_query($con, $show_producto);
 				$productos= mysqli_fetch_array($query_productos);
 				$date_added=date("Y-m-d H:i:s");
-				$codigo=$productos['codigo_producto'].$i.rand(0,100);
+				$codigo=$productos['codigo_producto'].$i.rand(0,10);
 				$nombre=$productos['nombre_producto'];
 				$descripcion=$productos['descripcion_producto'];
 				$proveedor=$proveedor_array['nombre_empresa'];
@@ -47,30 +49,19 @@
 
 				$sql_producto="INSERT INTO products (codigo_producto, nombre_producto, descripcion_producto, proveedor_producto, date_added, precio_producto, cantidad, id_categoria) VALUES ('$codigo', '$nombre', '$descripcion', '$proveedor', '$date_added', '$precio', '$cantidad', '$id_categoria')";
 				$query_producto=mysqli_query($con,$sql_producto);
-				
-				/*falta pasar la cantidad de cada producto para la compra y saber cuales son los ultimos datos insertados*/		
-				/*$query_compra="INSERT INTO compra_productos (id_producto, id_historial_compra, cantidad) VALUES ('$id_producto', '$id_historial_compra', '$cantidad')";
-				*/
-
-				
+				/*falta pasar la cantidad de cada producto para la compra y saber cuales son los ultimos datos insertados*/
+				$sql_show_products="SELECT id_producto as id FROM products WHERE codigo_producto='".$codigo."'";
+				$query_show_producto=mysqli_query($con,$sql_show_products);
+				$id_new_product=mysqli_fetch_array($query_show_producto)['id'];
+				/*var_dump($id_new_product);*/
+				$sql_compra="INSERT INTO compra_productos (id_producto, id_historial_compra, cantidad) VALUES ('$id_new_product', '$id_historial_compra', '$cantidad')";
+				$query_compra=mysqli_query($con,$sql_compra);
 			}
-		
-		
-		
-		
-		
-		/*$query_compra="INSERT INTO compra_productos (id_producto, id_historial_compra, cantidad) VALUES ('$id_producto', '$id_historial_compra', '$cantidad')";
-		*/
-		
 		} catch (Exception $e) {
-			
 			echo $e;
 		}
 
-
-		/*$query_new_insert = mysqli_query($con,$query_proveedor);*/
-
-		if (true){
+		if ($query_compra){
 			$messages[] = "Proveedor ha sido ingresada satisfactoriamente.";
 		} else{
 			$errors []= "Lo siento algo ha salido mal intenta nuevamente.".mysqli_error($con);
